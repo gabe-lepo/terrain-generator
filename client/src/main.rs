ea02feb5d45b610f3b0b1a4f299df11a22e8051f
gabe-lepo <gabelepoutre@gmail.com>
2026-04-12 02:24:45 -0400

    basic camera movement controls

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,4 @@
-// WARN: REMOVE THIS ALLOW BEFORE TESTING
+// WARN: REMOVE THESE ALLOWS
 // #![allow(dead_code, unused)]
 mod player;
 mod terrain;
@@ -16,29 +16,22 @@ fn main() {
     rl_handle.set_target_fps(60);
     rl_handle.disable_cursor();
 
-    // Camera and player setup
-    let mut camera = Camera3D::perspective(
-        Vector3::new(0.0, 10.0, 0.0),
-        Vector3::new(0.0, 10.0, -1.0),
-        Vector3::up(),
-        70.0,
-    );
+    // Player and terrain setup
     let mut player = Player::new(Vector3::new(0.0, 10.0, 0.0));
-
-    // Terrain
     let terrain = Terrain::new(&mut rl_handle, &rl_thread, 12345);
 
     // Main loop
     while !rl_handle.window_should_close() {
         // Update
         let dt = rl_handle.get_frame_time();
+        player.update(&rl_handle, dt);
 
         let mut draw_handle = rl_handle.begin_drawing(&rl_thread);
         draw_handle.clear_background(Color::DEEPSKYBLUE);
 
         // 3D drawing
         {
-            let mut draw3d_handle = draw_handle.begin_mode3D(camera);
+            let mut draw3d_handle = draw_handle.begin_mode3D(player.get_camera());
             terrain.render(&mut draw3d_handle);
         }
 
--- client/src/player.rs
+++ client/src/player.rs
@@ -25,6 +25,68 @@ impl Player {
         }
     }
 
+    pub fn update(&mut self, rl: &RaylibHandle, dt: f32) {
+        self.handle_mouse_look(rl);
+        self.handle_movement(rl, dt);
+    }
+
+    pub fn get_camera(&self) -> Camera3D {
+        Camera3D::perspective(
+            self.position,
+            self.position + self.get_forward_vec(),
+            Vector3::up(),
+            70.0,
+        )
+    }
+
+    // Private
+    fn handle_mouse_look(&mut self, rl: &RaylibHandle) {
+        let mouse_delta = rl.get_mouse_delta();
+        self.yaw -= mouse_delta.x * MOUSE_SENSITIVITY;
+        self.pitch -= mouse_delta.y * MOUSE_SENSITIVITY;
+        self.pitch -= self.pitch.clamp(-1.5, 1.5);
+    }
+
+    fn handle_movement(&mut self, rl: &RaylibHandle, dt: f32) {
+        let forward = self.get_forward_vec();
+        let right = self.get_right_vec();
+
+        if rl.is_key_down(KeyboardKey::KEY_W) {
+            self.position = self.position + forward * MOVE_SPEED * dt;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_S) {
+            self.position = self.position - forward * MOVE_SPEED * dt;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_A) {
+            self.position = self.position - right * MOVE_SPEED * dt;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_D) {
+            self.position = self.position + right * MOVE_SPEED * dt;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_SPACE) {
+            self.position.y += MOVE_SPEED * dt;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_LEFT_SHIFT) {
+            self.position.y -= MOVE_SPEED * dt;
+        }
+    }
+
+    fn get_forward_vec(&self) -> Vector3 {
+        Vector3::new(
+            self.pitch.cos() * self.yaw.sin(),
+            self.pitch.sin(),
+            self.pitch.cos() * self.yaw.cos(),
+        )
+    }
+
+    fn get_right_vec(&self) -> Vector3 {
+        Vector3::new(
+            (self.yaw - std::f32::consts::PI / 2.0).sin(),
+            0.0,
+            (self.yaw - std::f32::consts::PI / 2.0).cos(),
+        )
+    }
+
     // TODO: Methods
     // - pub fn handle_input(&mut self, rl: &RaylibHandle, dt: f32) {}
     // - pub fn update_physics(&mut self, dt: f32) {}
